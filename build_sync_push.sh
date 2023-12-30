70d003770a41f53aa7a1f0d9bb301f052ecadf90
tuananhbui89 <bta1489@gmail.com>
2023-12-30 15:27:21 +1100

    update

--- build_sync_push.sh
+++ build_sync_push.sh
@@ -1,6 +1,7 @@
 # bundle exec jekyll serve --draft
 bundle exec jekyll build --lsi
-rm -rf ../tuananhbui89.github.io/*
+rsync -av --progress --delete _site/blog/ ../tuananhbui89.github.io/blog
+rsync -av --progress --delete _site/projects/ ../tuananhbui89.github.io/projects
 rsync -av --progress _site/ ../tuananhbui89.github.io
 cd ../tuananhbui89.github.io
 git add .
